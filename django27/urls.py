7cb4e1abdd84c7f5aa623e9dd81a9129818ce192
Glassyflute <yuliya.bobir@gmail.com>
2022-12-12 22:41:10 +0600

    upgrade according to lesson 28.1

--- django27/urls.py
+++ django27/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from vacancies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
-    path('vacancy/', views.VacancyView.as_view()),
-    path('vacancy/<int:pk>/', views.VacancyDetailView.as_view()),
+    path('vacancy/', include('vacancies.urls')),
 ]
 
